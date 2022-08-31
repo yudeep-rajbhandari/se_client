26741f0aca0a36f05dfe26f5044e5c30153e38cf
Nises <nishandhojkarki@gmail.com>
2022-08-30 20:19:04 -0500

    test branch Nises

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          check <code>src/App.js</code> and save to reload.
         </p>
         
       </header>
