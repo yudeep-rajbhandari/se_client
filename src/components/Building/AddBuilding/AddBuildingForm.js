494c33fe8ba0524072a9fbc5001915831b0c8a41
tayba-mahee <mahee.noor.tayba@gmail.com>
2022-11-17 12:17:59 -0600

    Building form changed with material ui

--- src/components/Building/AddBuilding/AddBuildingForm.js
+++ src/components/Building/AddBuilding/AddBuildingForm.js
@@ -1,92 +1,87 @@
 // import { TextField } from "@mui/material";
 import TextField from '@mui/material/TextField';
-import { Box, Stack } from '@mui/system';
+import {Box, Stack} from '@mui/system';
+import AddBuilding from "./AddBuilding";
+import Button from "@mui/material/Button";
 
 export default function AddBuildingForm(props) {
   return (
-    <Box component="form"
-    sx={{
-      '& .MuiTextField-root': { m: 1, width: '25ch' },
-    }}
-    noValidate
-    autoComplete="off">
 
     <div>
       <h3>Add Building</h3>
-      <form onSubmit={props.onSubmit}>
-        <TextField   
-          required
-          id="name"
-          label="Building Name"> 
-        <input ref={props.nameRef} type="name" id="name"  />
-        /</TextField>
+      <Box onSubmit={props.onSubmit} component="form"
+           sx={{
+             '& .MuiTextField-root': {m: 1, width: '25ch'},
+           }}
+           noValidate
+           autoComplete="off"
+      >
+        <div>
 
-        <TextField   
-          id="floors"
-          label="Number of Floors" >
-        <input 
-        type="number"
-        ref={props.floorsRef} 
-        variant="filled"
-        id="floors"l
-        InputLabelProps={{
-          shrink: true,
-        }}
-        />
-        /</TextField>
-
-        <br/>
-        <br />
-        <label htmlFor="address"> Please add the building address below:</label>
-        <br />
-
-        <TextField 
-                label="Street">
-                <input 
-                ref={props.streetRef} 
-                type="street" 
-                id="street" 
-                />
-        /</TextField>
-
-        <br />
-        <TextField
-        id="city"
-        type="city"
-        label="City"
-        ref={props.cityRef}
-        />
+          <TextField
+            id="name"
+            label="Building Name"
+            inputRef={props.nameRef} type="name"
+          />
 
 
-        <br />
+          <TextField
+            id="floors"
+            label="Number of Floors"
+            type="number"
+            inputRef={props.floorsRef}
+            // variant="filled"
+            // InputLabelProps={{
+            //   shrink: true,
+            // }}
+          />
 
-        <TextField 
-                label="State">
-                <input 
-                ref={props.stateRef} 
-                type="state" 
-                id="state" 
-                />
-        /</TextField>
 
-        <br />
-        
-        <TextField 
-                label="Zip">
-                <input 
-                ref={props.zipRef} 
-                type="zip" 
-                id="zip" 
-                />
-        /</TextField>
+          <br/>
+          <br/>
+          <label htmlFor="address"> Please add the building address below:</label>
+          <br/>
 
-        <br />
+          <TextField
+            label="Street"
+            inputRef={props.streetRef}
+            type="street"
+            id="street"
+          />
 
-        <button type="submit">Add Building</button>
 
-        
-      </form>
+          <br/>
+          <TextField
+            id="city"
+            type="city"
+            label="City"
+            ref={props.cityRef}
+          />
+
+
+          <br/>
+
+          <TextField
+            label="State"
+            inputRef={props.stateRef}
+            type="state"
+            id="state"
+          />
+
+          <br/>
+
+          <TextField
+            label="Zip"
+            inputRef={props.zipRef}
+            type="zip"
+            id="zip"
+          />
+
+          <br/>
+
+          <Button type="submit">Add Building</Button>
+        </div>
+      </Box>
     </div>
-    </Box>
   );
 }
