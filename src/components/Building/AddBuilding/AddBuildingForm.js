cd3f8e55a8182a930d58afccfd7538d0695ffa20
tayba-mahee <mahee.noor.tayba@gmail.com>
2022-11-17 22:10:28 -0600

    handled the building

--- src/components/Building/AddBuilding/AddBuildingForm.js
+++ src/components/Building/AddBuilding/AddBuildingForm.js
@@ -1,6 +1,6 @@
 // import { TextField } from "@mui/material";
 import TextField from '@mui/material/TextField';
-import {Box, Stack} from '@mui/system';
+import { Box, Stack } from '@mui/system';
 import AddBuilding from "./AddBuilding";
 import Button from "@mui/material/Button";
 import SaveRoundedIcon from "@mui/icons-material/SaveRounded";
@@ -8,88 +8,73 @@ import SaveRoundedIcon from "@mui/icons-material/SaveRounded";
 
 export default function AddBuildingForm(props) {
   return (
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <div className='addBuilding'>
+        <h3>Add Building</h3>
+        <Box onSubmit={props.onSubmit} component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
 
-    <div>
-      <h3>Add Building</h3>
-      <Box onSubmit={props.onSubmit} component="form"
-           sx={{
-             '& .MuiTextField-root': {m: 1, width: '25ch'},
-           }}
-           noValidate
-           autoComplete="off"
-      >
-        <div>
+            <TextField
+              id="name"
+              label="Building Name"
+              inputRef={props.nameRef} type="name"
+            />
+            <TextField
+              id="floors"
+              label="Number of Floors"
+              type="number"
+              inputRef={props.floorsRef}
+            />
+            <label htmlFor="address"> <h3>Add address:</h3></label>
+            <TextField
+              label="Street"
+              inputRef={props.streetRef}
+              type="street"
+              id="street"
+            />
+            <TextField
+              id="city"
+              type="city"
+              label="City"
+              inputRef={props.cityRef}
+              defaultValue={"Waco"}
+            />
+            <TextField
+              label="State"
+              inputRef={props.stateRef}
+              type="state"
+              id="state"
+              defaultValue={"Texas"}
+            />
+            <TextField
+              label="Zip"
+              inputRef={props.zipRef}
+              type="zip"
+              id="zip"
+              defaultValue={"76706"}
+            />
 
-          <TextField
-            id="name"
-            label="Building Name"
-            inputRef={props.nameRef} type="name"
-          />
-
-
-          <TextField
-            id="floors"
-            label="Number of Floors"
-            type="number"
-            inputRef={props.floorsRef}
-
-          />
-
-
-          <br/>
-          <br/>
-          <label htmlFor="address"> Please add the building address below:</label>
-          <br/>
-
-          <TextField
-            label="Street"
-            inputRef={props.streetRef}
-            type="street"
-            id="street"
-          />
-
-
-          <br/>
-          <TextField
-            id="city"
-            type="city"
-            label="City"
-            inputRef={props.cityRef}
-            defaultValue={"Waco"}
-          />
-
-
-          <br/>
-
-          <TextField
-            label="State"
-            inputRef={props.stateRef}
-            type="state"
-            id="state"
-            defaultValue={"Texas"}
-          />
-
-          <br/>
-
-          <TextField
-            label="Zip"
-            inputRef={props.zipRef}
-            type="zip"
-            id="zip"
-            defaultValue={"76706"}
-          />
-
-          <br/>
-
-          <Button
-            startIcon={<SaveRoundedIcon />}
-            variant="contained"
-            type="submit"
-          >
-            Save Building
-          </Button>
-        </div>
-      </Box>
+          </div>
+        </Box>
+        <Button
+          startIcon={<SaveRoundedIcon />}
+          variant="contained"
+          type="submit"
+        >
+          Save Building
+        </Button>
+      </div>
     </div>
+
   );
 }
