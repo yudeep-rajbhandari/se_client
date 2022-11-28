ea6f17815c3d1fb74c5ce80af25ab059dc00a5fc
yudeep-rajbhandari <aezakmi@12345678>
2022-11-28 17:52:13 -0600

    server js update

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3000, () => console.log(`App running on 3000`));
+app.listen(8080, '0.0.0.0', () => console.log(`App running on 8080`));
