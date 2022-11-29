477b8fd983c8a12db1ba57e204751f42511de402
tayba-mahee <mahee.noor.tayba@gmail.com>
2022-11-29 10:23:37 -0600

    paths added

--- src/components/maps/leafindoor.component.js
+++ src/components/maps/leafindoor.component.js
@@ -584,7 +584,7 @@ if(props.id =='1'){
     pointList = path3;
 }
 
-const currentPath = allPaths.filter(p=>p.properties.name==='ElevatorE_319');
+const currentPath = allPaths.filter(p=>p.properties.name==='ElevatorE_317');
 console.log("hello world",currentPath)
     const aa = [];
 if(currentPath.length <1){
