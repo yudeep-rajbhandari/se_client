2b320a8b3aac7ea5bf0c21d48d2dcdcb9e7df1f0
tayba-mahee <mahee.noor.tayba@gmail.com>
2022-11-28 23:32:44 -0600

    room dashboard updated

--- src/components/AdminBoard/RoomDashboard/RoomDashboard.js
+++ src/components/AdminBoard/RoomDashboard/RoomDashboard.js
@@ -5,19 +5,25 @@ import React, { useState } from 'react';
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import BookableTable from "./BookableTable";
+import RoomtypeTable from "./RoomtypeTable";
 
 
 
 export default function RoomDashboard(props) {
   const [bookable, setBookable] = useState(false);
+  const [roomType, setRoomType] = useState(false);
   function showBookable() {
     setBookable(true);
+    setRoomType(false);
+  }
 
-
+  function showRoomType() {
+    setRoomType(true);
+    setBookable(false);
   }
   function hideTable() {
     setBookable(false);
-
+    setRoomType(false)
   }
   return (
     <div>
@@ -30,10 +36,18 @@ export default function RoomDashboard(props) {
           {bookable && <Button color="error" variant="text" onClick={() => hideTable()}>
             Hide Bookable
           </Button>}
+
+          {!roomType && <Button variant="text" onClick={() => showRoomType()}>
+            RoomType
+          </Button>}
+          {roomType && <Button color="error" variant="text" onClick={() => hideTable()}>
+            Hide RoomType
+          </Button>}
         </ButtonGroup>
       </div>
       <div>
         {bookable && <BookableTable rooms={props.rooms} />}
+        {roomType && <RoomtypeTable rooms={props.rooms} />}
       </div>
     </div>
   );
