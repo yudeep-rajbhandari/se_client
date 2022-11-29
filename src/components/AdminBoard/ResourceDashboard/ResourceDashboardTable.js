3b5ffac0a286f7c55bda90bf06691c58835989a3
tayba-mahee <mahee.noor.tayba@gmail.com>
2022-11-28 23:17:11 -0600

    resource dashboard updated

--- src/components/AdminBoard/ResourceDashboard/ResourceDashboardTable.js
+++ src/components/AdminBoard/ResourceDashboard/ResourceDashboardTable.js
@@ -2,22 +2,42 @@ import { TableBody, TableContainer } from "@mui/material";
 import { Table } from "@mui/material";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import TableCell from "@mui/material/TableCell";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
+import { styled } from "@mui/material/styles";
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
 
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+        border: 0,
+    },
+}));
 export default function ResourceDashboardTable(props) {
     return (<div>
         <TableContainer>
             <Table>
                 <TableHead>
                     <TableRow>
-                        <TableCell># Resource</TableCell>
+                        <StyledTableCell># Resource</StyledTableCell>
+
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     <TableRow>
-                        <TableCell>
+                        <StyledTableCell>
                             <Tooltip
                                 disableFocusListener
                                 disableTouchListener
@@ -32,7 +52,8 @@ export default function ResourceDashboardTable(props) {
                                     {props.resources.length}
                                 </Button>
                             </Tooltip>
-                        </TableCell>
+                        </StyledTableCell>
+
                     </TableRow>
                 </TableBody>
             </Table>
