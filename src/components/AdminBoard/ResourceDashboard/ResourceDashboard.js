5731d1ce13dbfd35c2611f17e7d5efe3f93f95c9
tayba-mahee <mahee.noor.tayba@gmail.com>
2022-11-28 23:17:11 -0600

    resource dashboard updated

--- src/components/AdminBoard/ResourceDashboard/ResourceDashboard.js
+++ src/components/AdminBoard/ResourceDashboard/ResourceDashboard.js
@@ -4,34 +4,58 @@ import Button from "@mui/material/Button";
 
 import ResourceDashboardTable from "./ResourceDashboardTable";
 import ResourceTypeTable from "./ResourceTypeTable";
-
+import ButtonGroup from "@mui/material/ButtonGroup";
+import WorkingConditionTable from './WorkingConditionTable';
 
 
 export default function ResourceDashoard(props) {
   const [resourceType, setResourceType] = useState(false);
+  const [workingCondition, setWorkingCondition] = useState(false);
 
   function showResourceType() {
     setResourceType(true);
-    console.log("divide by working condition")
+    setWorkingCondition(false)
+
   }
-  function hideResourceType() {
+  function hideTable() {
     setResourceType(false);
+    setWorkingCondition(false);
+  }
+
+  function showWorkingCondition() {
+    setWorkingCondition(true);
+    setResourceType(false)
+
+  }
+  function hideWorkingCondition() {
+    setWorkingCondition(false);
 
   }
   return (
     <div>
       <ResourceDashboardTable resources={props.resources} />
       <div >
-        {!resourceType && <Button variant="text" onClick={() => showResourceType()}>
-          Resource Type
-        </Button>}
+        <ButtonGroup variant="text" aria-label="text button group">
+          {!resourceType && <Button variant="text" onClick={() => showResourceType()}>
+            Resource Type
+          </Button>}
+          {resourceType && <Button color="error" variant="text" onClick={() => hideTable()}>
+            Hide Resource Type
+          </Button>}
+          {!workingCondition && <Button variant="text" onClick={() => showWorkingCondition()}>
+            Working Condition
+          </Button>}
+          {workingCondition && <Button color="error" variant="text" onClick={() => hideTable()}>
+            Hide Working Condition
+          </Button>}
+        </ButtonGroup>
 
-        {resourceType && <Button color="error" variant="text" onClick={() => hideResourceType()}>
-          Hide Resource Type
-        </Button>}
       </div>
       <div>
         {resourceType && <ResourceTypeTable resources={props.resources} />}
+      </div>
+      <div>
+        {workingCondition && <WorkingConditionTable resources={props.resources} />}
       </div>
 
     </div >
