0a99f264fb8e2d0a7ada9675d64e4a5f5d41bf5c
tayba-mahee <mahee.noor.tayba@gmail.com>
2022-11-29 12:18:34 -0600

    corridor paths added

--- src/components/maps/leafindoor.component.js
+++ src/components/maps/leafindoor.component.js
@@ -584,7 +584,7 @@ if(props.id =='1'){
     pointList = path3;
 }
 
-const currentPath = allPaths.filter(p=>p.properties.name==='ElevatorE_309.02');
+const currentPath = allPaths.filter(p=>p.properties.name==='ElevatorE_304');
 console.log("hello world",currentPath)
     const aa = [];
 if(currentPath.length <1){
