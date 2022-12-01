3ae7cee8fb49d5c7aedcefebf2bb2f1d47d46903
tayba-mahee <mahee.noor.tayba@gmail.com>
2022-12-01 01:22:49 -0600

    Unsolved: Can't add elevator-A to H399.02

--- src/components/maps/leafindoor.component.js
+++ src/components/maps/leafindoor.component.js
@@ -584,7 +584,7 @@ if(props.id =='1'){
     pointList = path3;
 }
 
-const currentPath = allPaths.filter(p=>p.properties.name==='ElevatorA_301');
+const currentPath = allPaths.filter(p=>p.properties.name==='ElevatorA_399.02');
 console.log("hello world",currentPath)
     const aa = [];
 if(currentPath.length <1){
