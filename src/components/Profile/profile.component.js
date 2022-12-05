29a37bafe13b336eb0a3c8d869e185079aa70a77
Pranish Bhagat <pranish_bhagat1@baylor.edu>
2022-12-04 22:50:12 -0600

    profile change

--- src/components/Profile/profile.component.js
+++ src/components/Profile/profile.component.js
@@ -32,19 +32,19 @@ export default class Profile extends Component {
           <div>
             <header className="jumbotron">
               <h3>
-                <strong>{currentUser.username}</strong> Profile
+                Welcome!! <strong> {currentUser.username}</strong>
               </h3>
             </header>
-            <p>
+            {/* <p>
               <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)}{" "}
               ...{" "}
               {currentUser.accessToken.substr(
                 currentUser.accessToken.length - 20
               )}
-            </p>
-            <p>
+            </p> */}
+            {/* <p>
               <strong>Id:</strong> {currentUser.id}
-            </p>
+            </p> */}
             <p>
               <strong>Email:</strong> {currentUser.email}
             </p>
